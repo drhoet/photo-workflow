76afbeb31e08e368a6c26b41b022bb36070bb257
drhoet <drhoet@users.noreply.github.com>
2022-12-18 22:48:34 +0800

    Small improvements to the shift time dialog

--- main/static/main/modules/shiftTimeDialog.js
+++ main/static/main/modules/shiftTimeDialog.js
@@ -4,18 +4,21 @@ export default {
     template: `
         <modal :showModal="showModal" @ok="shiftTime" @cancel="closeModal" id="shift-time-modal">
             <template v-slot:header>
-                <h3>Edit timezone</h3>
+                <h3>Edit time</h3>
             </template>
             <template v-slot:body>
                 <span>
                     <label>Adjust time by:</label>
                     <span class="selector">
+                        <button @click="decreaseValueByDay">-1d</button>
                         <button @click="decreaseValueByHour">-1h</button>
-                        <button @click="decreaseValue">-1m</button>
+                        <button @click="decreaseValueByMinute">-1m</button>
                         <span>{{ formattedTime }}</span>
-                        <button @click="increaseValue">+1m</button>
+                        <button @click="increaseValueByMinute">+1m</button>
                         <button @click="increaseValueByHour">+1h</button>
+                        <button @click="increaseValueByDay">+1d</button>
                     </span>
+                    <button @click="increaseValueBy">...</button>
                 </span>
             </template>
         </modal>
@@ -40,17 +43,29 @@ export default {
             this.$emit('update:time', this.minutes);
             this.closeModal();
         },
-        increaseValue() {
+        increaseValueByMinute() {
             this.minutes += 1;
         },
         increaseValueByHour() {
             this.minutes += 60;
         },
-        decreaseValue() {
+        increaseValueByDay() {
+            this.minutes += 60 * 24;
+        },
+        decreaseValueByMinute() {
             this.minutes -= 1;
         },
         decreaseValueByHour() {
             this.minutes -= 60;
+        },
+        decreaseValueByDay() {
+            this.minutes -= 60 * 24;
+        },
+        increaseValueBy() {
+            let inc = prompt("Type the number of minutes to increase the value by. Might be negative.", 0);
+            if(inc) {
+                this.minutes += parseInt(inc);
+            }
         }
     },
     watch: {
